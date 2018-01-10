0e3c46a471e459b4706262bd03bb0893f5ba885d
Samuel Mortenson <samuel.mortenson@acquia.com>
2018-01-09 20:30:27 -0800

    Remove all build files.

--- templates.js
+++
@@ -1,7 +0,0 @@
-// These templates are used by server side renderers to show an initial version
-// of the components provided by this library.
-
-module.exports = {
-  'tce-hero': require('./tce-hero/tce-hero.twig'),
-  /* generator-placeholder */
-};
